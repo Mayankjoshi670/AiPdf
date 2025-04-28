4a336717673a546b0f2d06d4d2ff2571d590a9b2
Mayank joshi <147848494+Mayankjoshi670@users.noreply.github.com>
2025-04-29 01:16:02 +0530

    file spliting into chunks

--- backend/worker.js
+++ backend/worker.js
@@ -1,9 +1,29 @@
 const {Worker} = require('bullmq') ; 
-
- 
+const {GoogleGenerativeAIEmbeddings } = require('@langchain/google-genai')
+const  { QdrantVectorStore } =  require("@langchain/qdrant");
+const {Document } = require("@langchain/core/documents");
+const  { PDFLoader } = require("@langchain/community/document_loaders/fs/pdf");
+const {RecursiveCharacterTextSplitter } = require("@langchain/textsplitters")
 const worker = new Worker('upload-file', async job => {
      console.log(`Job :` , job.data) ; 
-     const data = JSON.parse(job.data) ; 
+     const data  = JSON.parse(job.data) ; 
+     console.log(data) ; 
+     console.log("data loaded ")
+    
+     const path = data.path ;
+    //   load the pdf 
+    console.log(`path = ${path}`) 
+    const loader = new PDFLoader(path) ; 
+    const docs = await loader.load() ; 
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize : 400 , 
+        chunkOverlap : 50
+    }) ;
+    const splitDocs = await textSplitter.splitDocuments(docs) ; 
+    // console.log(docs)
+    // console.log(splitDocs)
+    console.log(docs.length)
+    console.log(splitDocs.length)
   }, { concurrency: 100  , 
     connection : {
         host: 'localhost' , 
