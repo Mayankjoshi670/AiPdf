a3d470f9f3b868b54b8c4b68c07530c75e0486c3
Mayank joshi <147848494+Mayankjoshi670@users.noreply.github.com>
2025-04-29 19:39:49 +0530

    store in vector db-qdrant

--- backend/worker.js
+++ backend/worker.js
@@ -2,6 +2,7 @@ const {Worker} = require('bullmq') ;
 const {GoogleGenerativeAIEmbeddings } = require('@langchain/google-genai')
 const  { QdrantVectorStore } =  require("@langchain/qdrant");
 const {Document } = require("@langchain/core/documents");
+const { QdrantClient } = require("@qdrant/js-client-rest");
 const  { PDFLoader } = require("@langchain/community/document_loaders/fs/pdf");
 const {RecursiveCharacterTextSplitter } = require("@langchain/textsplitters")
 const worker = new Worker('upload-file', async job => {
@@ -20,10 +21,25 @@ const worker = new Worker('upload-file', async job => {
         chunkOverlap : 50
     }) ;
     const splitDocs = await textSplitter.splitDocuments(docs) ; 
-    // console.log(docs)
-    // console.log(splitDocs)
-    console.log(docs.length)
-    console.log(splitDocs.length)
+     console.log(splitDocs)
+
+    //  now we create a vector embedding in quadrant db 
+
+
+    const client = new QdrantClient({url : `http://localhost:6333`}) ;
+    const embedding = new GoogleGenerativeAIEmbeddings({
+      model: "text-embedding-004", // 768 dimensions
+      apiKey : `AIzaSyAaPnIQZE5g0oUGnKY-6Dj_UgIab4XhPpU`
+
+    })
+    const vectorStore =  await QdrantVectorStore.fromExistingCollection( embedding, {
+      url : 'http://localhost:6333' , 
+      collectionName: "RAG-Ai-Pdf"
+    })
+
+    await vectorStore.addDocuments(splitDocs);
+    console.log("all docs are loaded to vector store ")
+
   }, { concurrency: 100  , 
     connection : {
         host: 'localhost' , 
---
+++ readme.md
@@ -0,0 +1,7 @@
+<!--  to install anyhtin in docker create its 
+services and run 
+docker compose up -d 
+ -->
+ <!-- http://localhost:6333/dashboard
+    here is your db data will shown 
+  -->
